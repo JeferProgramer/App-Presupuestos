4ff8461cf42dde1163cbe7bb2e9e63e583c222c1
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-02 15:26:38 -0500

    agregando la clase Ingreso competa

--- js/Ingreso.js
+++ js/Ingreso.js
@@ -1,11 +1,21 @@
+// class Ingreso extends Dato{
+//     static contadorIngresos = 0;
+
+//     constructor(descripcion, valor){
+//         super(descripcion, valor);
+//         this._id = ++Ingreso.contadorIngresos;
+//     }
+//     get id(){
+//         return this._id;
+//     }
+// }
 class Ingreso extends Dato{
     static contadorIngresos = 0;
-
     constructor(descripcion, valor){
         super(descripcion, valor);
         this._id = ++Ingreso.contadorIngresos;
     }
     get id(){
-        return this._id;
+        return this._id
     }
 }
