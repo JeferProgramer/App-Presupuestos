1a5be35483fa7cdf4566da462c5af21533d915d9
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-06 20:18:10 -0500

    quitando lo inecesario de app

--- js/Ingreso.js
+++ js/Ingreso.js
@@ -1,14 +1,4 @@
-// class Ingreso extends Dato{
-//     static contadorIngresos = 0;
 
-//     constructor(descripcion, valor){
-//         super(descripcion, valor);
-//         this._id = ++Ingreso.contadorIngresos;
-//     }
-//     get id(){
-//         return this._id;
-//     }
-// }
 class Ingreso extends Dato{
     static contadorIngresos = 0;
     constructor(descripcion, valor){
