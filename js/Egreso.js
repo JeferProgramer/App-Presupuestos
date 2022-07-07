2ab76d6353008291d8479a30c0a6e303ba3ffd37
JeferProgramer <jefer.talento.11@gmail.com>
2022-07-06 20:17:24 -0500

    quitando lo inecesario de egreso

--- js/Egreso.js
+++ js/Egreso.js
@@ -1,14 +1,4 @@
-// class Egreso extends Dato{
-//     static contadorEgresos = 0;
 
-//     constructor(descripcion, valor){
-//         super(descripcion, valor);
-//         this._id = ++Egreso.contadorEgresos;
-//     }
-//     get id(){
-//         return this._id;
-//     }
-// }
 class Egreso extends Dato{
     static contadorEgresos = 0;
     constructor(descripcion,valor){
